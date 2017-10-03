422cab40357a1602711bf413c3e21db1e62b61da
Michael Daniloff <mdaniloff@DocAuto.com>
2017-10-03 08:59:18 -0400

    Init commit

---
+++ SpanishVerbsApp/Bootstrapper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Practices.Unity;
+using Prism.Unity;
+using Verbs.DemoApp.Views;
+using System.Windows;
+
+namespace Verbs.DemoApp
+{
+    internal class Bootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void InitializeShell()
+        {
+            Application.Current.MainWindow.Show();
+        }
+    }
+}
---
+++ Verbs.Infrastructure/RegionNames.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Verbs.Infrastructure
+{
+    public class RegionNames
+    {
+        public static string StatusBarRegion = "StatusBarRegion";
+        public static string ContentRegion = "ContentRegion";
+        public static string OptionsRegion = "OptionsRegion";
+    }
+}
