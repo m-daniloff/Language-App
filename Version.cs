df11ca962320f076fee1a9c97204f39859bee594
Michael Daniloff <michael.daniloff@gmail.com>
2018-01-31 11:47:16 -0500

    Versioning

---
+++ Version.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+
+// The shared build version for all  Assemblies
+[assembly: AssemblyVersion("1.0.1.0024")]
+[assembly: AssemblyFileVersion("1.0.1.0024")]
+[assembly: AssemblyInformationalVersion("1.0.1.0024")]
